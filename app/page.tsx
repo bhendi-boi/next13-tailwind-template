268ff1e7978f99d398e7d5704cd5b5edf3b706d4
bhendi-boi <bhendiisgreen@gmail.com>
2023-02-26 16:40:37 +0530

    added link to github

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,17 @@ const page = () => {
   return (
     <main className="grid min-h-screen place-items-center">
       <div>
-        <h1 className="mb-4 text-5xl text-blue-400">Hello world 👋</h1>
+        <h1 className="mb-8 text-5xl font-bold text-green-400">
+          Hello world 👋
+        </h1>
         <p className="text-zinc-500">
           If you found this template useful give it a star on{' '}
-          <a href="https://github.com/bhendi-boi/next13-tailwind-template">
+          <a
+            target="_blank"
+            rel="norefer"
+            href="https://github.com/bhendi-boi/next13-tailwind-template"
+            className="text-xl text-blue-600 hover:text-blue-600/80"
+          >
             github
           </a>
           .
